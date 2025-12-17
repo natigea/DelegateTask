b793e78b9c53f483f71097827c35e52bf00ce623
natigea <natigea@code.edu.az>
2025-12-17 23:52:38 +0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,48 +1,95 @@
 ﻿namespace FuncTask4
 {
-    internal class Program
+    internal class Program 
     {
         static void Main(string[] args)
         {
-            List<Telebe> telebeler = new List<Telebe>
-            {
-                new Telebe { Name = "Gulnare",    Price = 99, Age = 24},
-                new Telebe { Name = "Ferid",  Price = 92, Age = 22 },
-                new Telebe { Name = "Elbrus",  Price = 60, Age = 21 },
-                new Telebe { Name = "Emil H",   Price = 90, Age = 40 },
-                new Telebe { Name = "Nezrin",  Price = 45, Age = 29 },
-                new Telebe { Name = "Murad",  Price = 68, Age = 22},
-                new Telebe { Name = "Natig", Price = 95, Age = 18 }
-            };
+            #region task 1
+            //int x = 200;
+            //int y = 40;
 
-            Predicate<Telebe> qiymeti70 = x => x.Price > 70;
-            Predicate<Telebe> yasi20 = x => x.Age < 20;
-            Predicate<Telebe> adiA = x => x.Name.StartsWith("A");
+            //MathOperation operation;
 
-            List<Telebe> Qiymet = telebeler.FindAll(qiymeti70);
-            List<Telebe> Yas = telebeler.FindAll(yasi20);
-            List<Telebe> Ad = telebeler.FindAll(adiA);
+            //operation = MathOperations.Add;
+            //Console.WriteLine($"add: {operation(x, y)}");
 
-            Func<Telebe, string> Telebe = x =>
-                $"Ad: {x.Name}, Qiymet: {x.Price}, Yas: {x.Age}";
+            //operation = MathOperations.Subtract;
+            //Console.WriteLine($"cixma: {operation(x, y)}");
 
-            Action<List<Telebe>> telebee = list =>
-            {
-                foreach (var x in list)
-                {
-                    Console.WriteLine(Telebe(x));
-                }
-                Console.WriteLine("");
-            };
+            //operation = MathOperations.Multiply;
+            //Console.WriteLine($"vurma: {operation(x, y)}");
 
-            Console.WriteLine($"Qiymeti 70-den yuxari olanlar:");
-            telebee(Qiymet);
+            //operation = MathOperations.Divide;
+            //Console.WriteLine($"bolme: {operation(x, y)}");
+            #endregion
 
-            Console.WriteLine("Yasi 20-den kicik olanlar:");
-            telebee(Yas);
+            #region task 2
+            Action<string> salam;
 
-            Console.WriteLine("Adi 'A' ilee baslayanlar:");
-            telebee(Ad);
+            salam = Task2.SalamAz;
+            salam("Natig");
+
+            salam = Task2.SalamEn;
+            salam("Natig");
+
+            salam = Task2.SalamTr;
+            salam("Natig");
+
+            #endregion
+
+            #region task 3
+            //Func<int, bool> isEven = number => number % 2 == 0;
+
+            //Func<int, bool> isPositive = number => number > 0;
+
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    Console.WriteLine($"eded: {i}, Cutdur: {isEven(i)}, Musbetdir: {isPositive(i)}");
+            //}
+            #endregion
+
+            #region task 4
+            //List<Telebe> telebeler = new List<Telebe>
+            //    {
+            //    new Telebe { Name = "Gulnare",    Price = 99, Age = 24},
+            //    new Telebe { Name = "Ferid",  Price = 92, Age = 22 },
+            //    new Telebe { Name = "Elbrus",  Price = 60, Age = 21 },
+            //    new Telebe { Name = "Emil H",   Price = 90, Age = 40 },
+            //    new Telebe { Name = "Nezrin",  Price = 45, Age = 29 },
+            //    new Telebe { Name = "Murad",  Price = 68, Age = 22},
+            //    new Telebe { Name = "Natig", Price = 95, Age = 18 }
+            //    };
+
+            //Predicate<Telebe> qiymeti70 = x => x.Price > 70;
+            //Predicate<Telebe> yasi20 = x => x.Age < 20;
+            //Predicate<Telebe> adiA = x => x.Name.StartsWith("A");
+
+            //List<Telebe> Qiymet = telebeler.FindAll(qiymeti70);
+            //List<Telebe> Yas = telebeler.FindAll(yasi20);
+            //List<Telebe> Ad = telebeler.FindAll(adiA);
+
+            //Func<Telebe, string> Telebe = x =>
+            //    $"Ad: {x.Name}, Qiymet: {x.Price}, Yas: {x.Age}";
+
+            //Action<List<Telebe>> telebee = list =>
+            //{
+            //    foreach (var x in list)
+            //    {
+            //        Console.WriteLine(Telebe(x));
+            //    }
+            //    Console.WriteLine("");
+            //};
+
+            //Console.WriteLine($"Qiymeti 70-den yuxari olanlar:");
+            //telebee(Qiymet);
+
+            //Console.WriteLine("Yasi 20-den kicik olanlar:");
+            //telebee(Yas);
+
+            //Console.WriteLine("Adi 'A' ilee baslayanlar:");
+            //telebee(Ad);
+        #endregion
         }
     }
 }
+
---
+++ Task2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FuncTask4
+{
+    internal class Task2
+    {
+
+        public static void SalamAz(string ad)
+        {
+            Console.WriteLine("Salam, " + ad);
+        }
+
+        public static void SalamEn(string ad)
+        {
+            Console.WriteLine("Hello, " + ad);
+        }
+
+        public static void SalamTr(string ad)
+        {
+            Console.WriteLine("Merhaba, " + ad);
+        }
+    }
+}
